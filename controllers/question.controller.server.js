dc7bf64d97dee08dc2f111958942757d00fd7ac2
Tina Wu <tinawu0603@hotmail.com>
2020-04-13 14:01:43 -0400

    fix question

--- controllers/question.controller.server.js
+++ controllers/question.controller.server.js
@@ -3,7 +3,6 @@ const questionService = require('../services/question.service.server')
 module.exports = function (app) {
     app.get('/api/quizzes/:qid/questions', (req, res) => {
         const qid = req.params['qid']
-        questionService.findQuestionsForQuiz(qid)
-            .then(questions => res.json(questions))
+        res.send(questionService.findQuestionsForQuiz(qid))
     })
 }
